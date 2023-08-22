08b85e50c2f01c724d1bc3b77a686383f1e963ab
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2023-08-23 01:21:30 +0300

    feat: initial implementation of product-list component - add product.service - add ngOnInit method that fetches the products - add markup to display list of products

---
+++ frontend/src/app/services/product.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Product } from '../common/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getProductsList(): Observable<Product[]> {
+    return this.httpClient
+      .get<GetReponse>(this.baseUrl)
+      .pipe(map((response) => response._embedded.products));
+  }
+}
+
+interface GetReponse {
+  _embedded: { products: Product[] };
+}
